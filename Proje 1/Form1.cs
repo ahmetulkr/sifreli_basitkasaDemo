1ee36b9bacb1a0aeab52c988524e4d9a307ebeb8
Ahmet Ülker <aulkerhm@gmail.com>
2024-05-14 09:56:05 +0300

    Proje dosyası ekle.

---
+++ Proje 1/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Proje_1
+{
+    public partial class Form1 : Form
+    {
+
+        int sifre = 2356;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text=="")
+            {
+                MessageBox.Show("Sifreyi giriniz");
+
+
+            }
+
+            else if (textBox2.Text=="1881")
+            {
+                MessageBox.Show("Tebrikler başarılı giriş yaptınız");
+
+                this.Hide();
+                Form2 frm = new Form2();
+                frm.Show();
+            }
+
+
+            
+
+        }
+
+
+
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                       
+           textBox2.Text += "1";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+       textBox2.Text += "2";
+    }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+       textBox2.Text += "3";
+    }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+        textBox2.Text += "4";
+    }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+        textBox2.Text += "5";
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox2.Text += "6";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox2.Text += "7";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox2.Text += "8";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox2.Text += "9";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox2.Text += "0";
+
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+        }
+    }
+}
---
+++ Proje 1/Form2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Proje_1
+{
+
+   
+    public partial class Form2 : Form
+    {
+
+        
+       
+        
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamWriter A = new StreamWriter(Application.StartupPath + "\\form\\" + "nots.txt");
+           
+            A.WriteLine(textBox1.Text + " " );
+
+            A.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StreamReader streamReader = File.OpenText(Application.StartupPath + "\\form\\" + "nots.txt");
+            string metin;
+            while ((metin = streamReader.ReadLine()) != null)
+
+            {
+                textBox1.Text = metin;
+
+            }
+            streamReader.Close();
+        }
+    }
+}
